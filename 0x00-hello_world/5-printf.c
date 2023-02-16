a3c12d6ecbeb6d0f4f4ce46b48658e2b9ac78af9
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-16 03:03:13 -0800

    dsdsd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - another function i wanna write for some reason
+ * Return: 0 (Success) 
+**/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
