19e7c5927c6a1608e13d13f831934e5de976983c
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-16 03:06:08 -0800

    dsdsd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - another function i wanna write for some reason
- * Return: 0 (Success) 
-**/
+ * main - a fucntion that is importaint
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
