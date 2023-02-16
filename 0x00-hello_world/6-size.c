bd79c278df3e7507c24e5c2706d91b3f74baac71
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-16 03:32:49 -0800

    ddssd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main(void)
 	long long d;
 	float f;
 }
-printf("Size of a char: %lu byte(s)", (unsigned long) sizeof(a));
-printf("Size of a int: %lu byte(s)", (unsigned long) sizeof(b));
-printf("Size of a long: %lu byte(s)", (unsigned long) sizeof(c));
-printf("Size of a long long: %lu byte(s)", (unsigned long) sizeof(d));
-printf("Size of a float: %lu byte(s)", (unsigned long) sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
