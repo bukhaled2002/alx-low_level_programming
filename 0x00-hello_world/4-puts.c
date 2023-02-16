ecb9323ac7f07c2d212ec891a91e7f5f6937e982
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-16 02:30:17 -0800

    dsds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*this is discription
+/*
+ * this is discription
  * asas
  * asas
  * sasaas
 */
 int main(void)
 {
+	/*this is for function*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
