018ddcf43e4fe0021c0ee40640f2c6983c2ccda4
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-16 03:29:47 -0800

    ddssd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - some code that is hard
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float f;
+}
+printf("Size of a char: %lu byte(s)", (unsigned long) sizeof(a));
+printf("Size of a int: %lu byte(s)", (unsigned long) sizeof(b));
+printf("Size of a long: %lu byte(s)", (unsigned long) sizeof(c));
+printf("Size of a long long: %lu byte(s)", (unsigned long) sizeof(d));
+printf("Size of a float: %lu byte(s)", (unsigned long) sizeof(f));
+return (0);
