6643a1f11dbe48e2cc111cbe4aa569dd44abf7ae
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-17 02:25:37 -0800

    ssddssd

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,16 +3,17 @@
  * main - this is comment
  * Return: 0 (Success)
  */
-void main(int)
+int main(void)
 {
 int k;
 char i;
-for(k=0;k<10;k++){
-putchar(k);
-}
-for(i='a';i<='f';i++){
-putchar(i);
-}
+
+for (k = 0 ; k < 10; k++)
+	putchar(k + '0');
+
+for (i = 'a'; i <= 'f'; i++)
+	putchar(i);
+
 putchar('\n');
 return (0);
 }
