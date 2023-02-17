d260993e822d45b3fed633fee1836f3a56a4d11a
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-17 02:30:08 -0800

    dsdssd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
@@ -8,16 +9,17 @@
  */
 int main(void)
 {
-      	int n;
-	for(n=0;n<10;n++)
+	int n;
+
+	for (n = 0; n < 10; n++)
 	{
-		putchar(n);
-		if(n!=9)
+		putchar(n + '0');
+		if (n != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	pucthar('\n');
- return (0);
+return (0);
 }
