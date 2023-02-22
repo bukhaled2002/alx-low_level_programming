2c0789773c285b39ca6f61c7dc210c6f2ba96d78
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-22 01:22:36 -0800

    sddssdddddddddddddd

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,38 +1,12 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * times_table - any description
+ * add - any description
+ * @n1: adafffasaafd
+ * @n2: assssssssssssss
  * Return: 1 (Success)
  */
-void times_table(void)
+int add(int n1, int n2)
 {
-int i;
-int j;
-
-for (i = 0; i <= 9; i++)
-{
-	for (j = 0; j <= 9; j++)
-	{
-		if (j != 0)
-		{
-			_putchar(',');
-			if (j * i < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-				_putchar(' ');
-		}
-		if (j * i < 10)
-			_putchar(j * i + '0');
-		else 
-		{
-			_putchar(j * i / 10 + '0');
-			_putchar(j * i % 10 + '0');
-		}
-		if (j == 9)
-			_putchar('\n');
-	}
-}
+	return (n1 + n2);
 }
