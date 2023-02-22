e5e2fd3505e5342cb3f6e3982acf10be2c05447d
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-22 00:09:22 -0800

    sddssdddddddddddddd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,11 +9,11 @@ void times_table(void)
 int i;
 int j;
 
-for (i = 0; i < 9; i++)
+for (i = 0; i <= 9; i++)
 {
-	for (j = 0; j < 9; j++)
+	for (j = 0; j <= 9; j++)
 	{
-	_putchar(i * j);
+	_putchar(i * j + '0');
 	_putchar(j == 9 ? '0' :' ');
 	_putchar(j == 9 ? '\n': ',');
 	}
