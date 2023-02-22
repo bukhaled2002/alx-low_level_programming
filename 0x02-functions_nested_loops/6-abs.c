1f152f3845c86acf917faa05cdd7214e83d32b3a
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-21 22:38:54 -0800

    dssdsd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _abs - any description
+ * @n: balssalsas adsa
+ * Return: 1 (Success)
+ */
+int _abs(int n)
+{
+if (n < 0 )
+	return (n * -1);
+else
+	return (n);
+}
