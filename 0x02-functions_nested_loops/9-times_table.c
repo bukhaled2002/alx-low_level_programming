b27e710e55e070c8868d38b9b6812c3e3afd14cf
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-22 00:50:13 -0800

    sddssdddddddddddddd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,11 +14,11 @@ for (i = 0; i <= 9; i++)
 	for (j = 0; j <= 9; j++)
 	{
 		if (j != 0)
-			_putchar(i * j < 9 ? ' ' : i * j / 10 + '0');
+			_putchar(i * j <= 9 ? ' ' : i * j / 10 + '0');
 		else
-			_putchar(i * j < 9 ? '0' : i * j / 10 + '0');
-	_putchar(i * j < 9 ? i * j + '0' : i * j % 10 + '0');
-	if(j != 9)
+			_putchar(i * j <= 9 ? '0' : i * j / 10 + '0');
+	_putchar(i * j <= 9 ? i * j + '0' : i * j % 10 + '0');
+	if (j != 9)
 		_putchar(',');
 	_putchar(j == 9 ? '\n' : ' ');
 	}
