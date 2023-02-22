784dde6c084bff85b21ddaa6c7954140ddb7c604
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-21 22:01:54 -0800

    dssdsd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 /**
  * print_alphabet_x10 - any description
- *
  * Return: 0 (Success)
  */
 void print_alphabet_x10(void)
@@ -10,12 +9,8 @@ void print_alphabet_x10(void)
 int i;
 char c;
 
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}	
-_putchar('\n');
+	print_alphabet();
 }
 }
