27b5e3d01709e664f978044da59ade33c02994e9
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-21 22:45:48 -0800

    dssdsd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_last_digit - any description
+ * @n: balssalsas adsa
+ * Return: 1 (Success)
+ */
+int print_last_digit(int n)
+{
+int i = n % 10;
+
+_putchar(i);
+return i
+}
