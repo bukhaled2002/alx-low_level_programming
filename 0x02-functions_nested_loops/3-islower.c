469deaf1b295c7dc71965463e333176384cc3938
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-21 22:17:43 -0800

    dssdsd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,17 +4,13 @@
  * _islower - any description
  * @c: balssalsas adsa
  * Return: 1 (Success)
- * Return: 0 (Fail)
  */
 int _islower(int c)
 {
 char i = c + '0';
 
-if (i >= 'a' || i<= 'z'){
-return (1);
-}
+if (c >= 'a' || c <= 'z')
+	return (1);
 else
-{
 	return (0);
-}
 }
