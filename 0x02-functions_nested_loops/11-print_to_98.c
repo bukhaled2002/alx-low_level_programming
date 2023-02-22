17a390a692bd8cddcb53a23ccd8951c95c9ced92
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-22 02:12:23 -0800

    sddssdddddddddddddd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,12 @@
 /**
  * print_to_98 - any description
  * @n: adafffasaafd
- * Return: 1 (Success)
  */
 void print_to_98(int n)
 {
 int dif;
 int i;
+
 
 	if (n > 98)
 		dif = -1;
@@ -19,9 +19,9 @@ int i;
 	{
 		if (i != 98)
 		{
-			if(i < 100)
+			if (i < 100)
 			{
-			if(i < 10)
+			if (i < 10)
 			_putchar(i + '0');
 			else
 			{
@@ -41,5 +41,4 @@ int i;
 	}
 	_putchar('9');
 	_putchar('8');
-
-}
+	_putchar('\n');
