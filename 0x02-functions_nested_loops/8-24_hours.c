739f8292b0e360b625cb41033e775873e012123e
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-22 00:00:20 -0800

    sddssdddddddddddddd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ int j;
 
 for (i = 0; i <= 23; i++)
 {
-	for (j= 0; j <= 59; j++)
+	for (j = 0; j <= 59; j++)
 	{
 		_putchar(i < 9 ? 0 + '0' : i / 10 + '0');
 		_putchar(i < 9 ? i + '0' : i % 10 + '0');
 		_putchar(':');
-		_putchar(j < 9 ? 0 + '0' : i / 10 + '0');
+		_putchar(j < 9 ? 0 + '0' : j / 10 + '0');
 		_putchar(j < 9 ? j + '0' : j % 10 + '0');
 		_putchar('\n');
 }
