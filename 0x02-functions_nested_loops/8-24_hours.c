2ab732c84484b79daf41033b2401e3c572cd9947
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-21 23:13:51 -0800

    sdsd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,19 +6,19 @@
  */
 void jack_bauer(void)
 {
-char i;
-char j;
+int i;
+int j;
 
 for (i = 0; i <= 23; i++)
 {
 	if (i < 10)
 	{
 		_putchar('0');
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	else
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar(':');
 	for (j = 0; j <= 59; j++)
@@ -26,11 +26,11 @@ for (i = 0; i <= 23; i++)
 		if (j < 10)
 		{
 			_putchar('0');
-			_putchar(j);
+			_putchar(j + '0');
 		}
 		else
 		{
-			_putchar(j);
+			_putchar(j + '0');
 		}
 	}
 	_putchar('\n');
