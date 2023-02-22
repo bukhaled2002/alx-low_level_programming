686e4d53c66b38ffeb7c3a52c454a3590ef0db44
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-21 23:07:01 -0800

    sdsd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ for (i = 0; i <= 23; i++)
 	if (i < 10)
 	{
 		_putchar('0');
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	else
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar(':');
 	for (j = 0; j <= 59; j++)
@@ -26,11 +26,11 @@ for (i = 0; i <= 23; i++)
 		if (j < 10)
 		{
 			_putchar('0');
-			_putchar(j);
+			_putchar(j + '0');
 		}
 		else
 		{
-			_putchar(j);
+			_putchar(j + '0');
 		}
 	}
 	_putchar('\n');
