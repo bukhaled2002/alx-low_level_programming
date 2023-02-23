b91fc263191b59dc93d6efddd322adb1cd920998
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 07:49:52 -0800

    dsds

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ for (j = 0; i < 10; j++)
 {
 	for (i = 0; i < 14; i++)
 	{
-		_putchar(i < 10 ? i + '0' : i / 10 + '0');
 		if (i >= 10)
-			_putchar(i % 10 + '0');
+			_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
 }
