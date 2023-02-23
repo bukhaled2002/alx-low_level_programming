faeb053a7686f4e432c384e65a22a5b099a4507a
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 07:22:08 -0800

    dsdssd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,15 @@
 
 /**
  * print_numbers - sadds akfasfas madsksa
- * Return: 1 Success 0 else
+ * Return: 1 Success
  */
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 		_putchar(i + '0');
+
+	}
 }
