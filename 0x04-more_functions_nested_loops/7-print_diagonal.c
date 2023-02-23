617738f84d922db51183398dd96eddac8ad49aef
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 08:32:19 -0800

    sddssd

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,15 @@ void print_diagonal(int n)
 	int i, j;
 if (n > 0)
 {
-	for (j = 0; j < n; j++)
+for (j = 0; j < n; j++)
 	{
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < j ; i++)
 		_putchar(' ');
 	_putchar('\\');
 	_putchar('\n');
 	}
 }
-	else
+else
 	_putchar('\n');
 
 }
