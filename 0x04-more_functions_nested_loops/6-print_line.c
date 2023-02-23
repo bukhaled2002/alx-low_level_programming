3053a6441119c697d42e2237fc757a494706339b
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 08:00:21 -0800

    dsds

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - sadds akfasfas madsksa
+ * @n: sjsdsajskldsadasajkadsjknadsjndsa
+ * Return: 1 Success 0 else
+ */
+int print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
