95f2c774a107c2bd276403b606a61de614548f35
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 07:19:58 -0800

    dsdssd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - sadds akfasfas madsksa
+ * Return: 1 Success 0 else
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+}
