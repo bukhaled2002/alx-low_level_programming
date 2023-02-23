d4db0663b9fa869f8b4849e180cee945202256fe
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 08:52:05 -0800

    sdds

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - sadds akfasfas madsksa
+ * @n: sjsdsajskld sadasajkadsjk nadsjndsa
+ * Return: 1 Success 0 else
+ */
+void print_square(int n)
+{
+	int i, j;
+if (n > 0)
+{
+for (j = 0; j < n; j++)
+{
+for (i = 0; i < n ; i++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+else
+	_putchar('\n');
+
+}
