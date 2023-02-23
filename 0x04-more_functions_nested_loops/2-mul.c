fcf0ac93230a6ac9d0dba0839c4bd20794e31dba
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 07:16:28 -0800

    dsdssd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  mul- sadds akfasfas madsksa
+ * @a: sjsdsajskldsadas
+ * @b: assdkjdajkadsjknadsjndsa
+ * Return: 1 Success 0 else
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
