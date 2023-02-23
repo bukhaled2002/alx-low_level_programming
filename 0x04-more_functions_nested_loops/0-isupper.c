b741381efbcbdd9ef2ee90e3ab37a54ac7effb87
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 07:08:20 -0800

    dsdssd

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
  * _isupper - sadds akfasfas madsksa
  * @c: sjsdsajskldsadas
- * Return: 1 Success
+ * Return: 1 Success 0 else
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 98)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
