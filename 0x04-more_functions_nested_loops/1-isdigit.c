1cf4804fbdbfeb49ba14f8a1fa10d33db2b1e581
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 07:12:35 -0800

    dsdssd

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - sadds akfasfas madsksa
+ * @c: sjsdsajskldsadas
+ * Return: 1 Success 0 else
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
