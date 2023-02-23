f1c28cc91a4a3241d651fdcedc127e9307d5b336
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 09:48:36 -0800

    sdsds

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - assdadsadsasdasdads
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+}
