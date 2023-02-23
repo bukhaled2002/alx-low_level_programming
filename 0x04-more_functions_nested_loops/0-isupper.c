2920768c6010b6ae2e9232c184bc901a192b81b4
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-23 06:52:13 -0800

    dsdssd

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - sadds akfasfas madsksa
+ * @c: sjsdsajskldsadas
+ * Return: 1 Success
+ */
+int _isupper(int c)
+{
+	char ch;
+	int v;
+
+	ch = c + '0';
+	if (ch >= 'A' && ch <= 'Z')
+		v = 1;
+	else
+		v = 0;
+return (v);
+
+}
