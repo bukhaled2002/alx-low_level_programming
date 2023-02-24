3a4c70b2b2f0a8d9ab473357a6f6e931e21b7710
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-24 00:09:00 -0800

    dsds

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
-#include <inttypes.h>
-#include <stdbool.h>
-#include <stdint.h>
+#include <math.h>
 /**
  * main - assdadsadsasdasdads
  * Return: 0 success
  */
 int main(void)
 {
-	long int i; long devider; bool f; long loop;
+	long int i; long devider; long loop;
 
 	i = 612852475143;
-	f = true;
-	devider = i;
-	while (f)
+	if (i % 2 == 0)
+		devider = i / 2;
+	for (loop = 3; loop < i / 2; loop++)
 	{
-		devider--;
-		if (i % devider == 0)
+		if (i % loop == 0)
 		{
-			for (loop = 2; loop < i; loop++)
-			{
-				if (i % loop != 0)
-				{
-					printf("%ld",i);
-					f = false;
-				}
-			}
+			devider = i / loop;
+			printf("%ld",devider);
+			break;
 		}
-
 	}
 	printf("\n");
 return (0);
