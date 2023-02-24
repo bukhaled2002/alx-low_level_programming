8280efdf12d669fe14d11d5d0fce0ac86619e2d3
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-24 00:10:36 -0800

    dsds

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-	long int i; long devider; long loop;
+	long int i;
+	long int devider;
+	long int loop;
 
 	i = 612852475143;
 	if (i % 2 == 0)
@@ -16,7 +18,7 @@ int main(void)
 		if (i % loop == 0)
 		{
 			devider = i / loop;
-			printf("%ld",devider);
+			printf("%ld", devider);
 			break;
 		}
 	}
