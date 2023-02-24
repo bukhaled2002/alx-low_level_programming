28d8c6e44c6a0070246b858e591913721df238a4
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-24 00:45:07 -0800

    dsds

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,8 +7,17 @@
  */
 void print_number(int n)
 {
+	if (n < 0)
+	{
+		n = -1 * n;
+		_putchar('-');
+	}
+	if (n > 10000)
+		_putchar(n / 10000 + '0');
+	if (n > 1000)
+		_putchar((n / 1000) % 100 + '0');
 	if (n > 100)
-		_putchar(n / 100 + '0');
+		_putchar((n / 100) % 10 + '0');
 	if (n > 10)
 		_putchar((n / 10) % 10 + '0');
 	_putchar(n % 10);
