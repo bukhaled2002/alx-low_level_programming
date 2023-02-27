8d1265ba14c672895ba4efeef54c9e81bb010a12
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-27 10:16:15 -0800

    sds

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,26 @@
 #include "main.h"
 /**
- * print_rev - returns the length of a string
+ * print_rev - imprime en reversa
  * @s: string
+ * return: 0
  */
 void print_rev(char *s)
-{	int count,o;
-
+{
+	int longi = 0;
+	int o;
 
 	while (*s != '\0')
 	{
-		count++;
+		longi++;
 		s++;
 	}
 	s--;
-	for (o = count; o > 0; o--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
+
