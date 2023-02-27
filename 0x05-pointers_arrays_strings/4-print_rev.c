0754ff35ad9ad9fe449e9877d2629ddb669fd980
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-27 10:14:17 -0800

    sds

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - returns the length of a string
+ * @s: string
+ */
+void print_rev(char *s)
+{	int count,o;
+
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	s--
+	for (o = count; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
