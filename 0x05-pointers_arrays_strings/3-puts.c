59b4dcdd167d4bfb730e3f4d06ad99ccf05988c1
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-27 09:29:38 -0800

    dsds

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - returns the length of a string
+ * @str: string
+ */
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		printf("%s",*s);
+		s++;
+	}
+	printf("\n");
+}
