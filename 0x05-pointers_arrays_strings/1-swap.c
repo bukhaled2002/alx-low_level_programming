d5e6100c4f7170be85b32ea4c5bf5867843ea148
MohamedKhaled <bakhaled.310@gmail.com>
2023-02-27 09:06:28 -0800

    second upload

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - aajadffd dskfkddf
+ * @a: adasdjnfsa jfjkdf dsfjdsf
+ * @b: asksa saksa ajas
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
