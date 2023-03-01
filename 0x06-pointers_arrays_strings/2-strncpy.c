cf13cea24e05c231bcf1f61b2dcae81705eef808
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-01 07:45:21 -0800

    ssd

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,28 @@
 #include "main.h"
 /**
- * _strncpy - concatenates two strings
+ * _strncpy - copy a string
  * @dest: input value
  * @src: input value
- * @n: n value
+ * @n: input value
  *
- * Return: void
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int j;
 
-	while (src[i] != '\0' & i < n)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		dest[j] = src[j];
+		j++;
 	}
-	while (i < n)
+	while (j < n)
 	{
-		dest[i] = '\0';
-		i++
+		dest[j] = '\0';
+		j++;
 	}
+
 	return (dest);
 }
+
