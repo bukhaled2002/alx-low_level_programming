7a3e1de1d4f8c42cff68107dcd581c7df5bf3bf9
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-01 08:45:42 -0800

    ssd

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s1[]="aAeEoOtTlL";
+	char s2[]="4433007711"
+		while (str[i] != '\0')
+		{
+			for (j = 0; j < 10; j++)
+			{
+				if (str[i] == s1[j])
+					str[i] = s2[j];
+			}
+		}
+	return (str);
+}
