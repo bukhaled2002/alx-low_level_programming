ebea607b5c44dd32e678f48982cd10b348386774
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-01 05:35:35 -0800

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * _strncat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * @n: n value
+ *
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' & j <= n)	
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j > n)
+		dest[i] = '\0';
+	return (dest);
+}
