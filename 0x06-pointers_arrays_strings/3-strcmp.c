55dfce0d7c47e5e5bb649633686e32e64306bdf0
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-01 07:59:19 -0800

    ssd

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: input value
  * @s2: input value
  *
- * Return: void
+ * Return: s1-s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] - s2[i] != 0)
 			return (s1[i] - s2[i]);
 		i++;
 	}
