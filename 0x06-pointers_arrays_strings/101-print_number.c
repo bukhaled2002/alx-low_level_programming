9ea1ea1c9ac121e8b7e589096eb169af3be0f77e
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-02 06:26:26 -0800

    fdf

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+void print_number(int n)
+{
+	int n1;
+
+	if (n < 0)
+	{
+		n1 = -1 * n;
+		_putchar('-');
+	}
+	if (n1 / 10 != 0)
+		print_numbers(n1 / 10);
+	_putchar (n1 % 10 + '0');
+}
+
