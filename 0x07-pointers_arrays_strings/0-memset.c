6974b971546082dbc260c5eb734af95e5da5e469
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-06 07:42:41 -0800

    first upload

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,9 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n / sizeof(char); i++)
-	s[i] = b;
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
 	return (s);
 }
