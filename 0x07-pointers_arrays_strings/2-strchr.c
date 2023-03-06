ee0533e304984ca5f8215dcecab58c01dbbbb03f
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-06 09:15:37 -0800

    first upload

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @c: the desired valu number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int r = n;
+
+	for (; i < r; i++)
+	{
+		if (s[i] == c)
+		{
+			return i;
+			break;
+		}
+		n--;
+	}
+		return (dest);
+}
