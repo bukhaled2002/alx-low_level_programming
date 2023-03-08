976fa4d6cf708c6dc8cbe0d7f95f7474af35047d
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-08 00:55:31 -0800

    sdds

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1+=a[i*size+i];
+	}
+	for (i = 1; i <= size; i++)
+	{
+		sum2+=a[i*size-i];
+	}
+}
