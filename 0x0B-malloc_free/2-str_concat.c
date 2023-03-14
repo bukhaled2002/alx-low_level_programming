5a1a18de4902085cb5d1863bd983b69eaaf327d6
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-14 09:30:11 -0700

    ffddf

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - create array of size size and assign char c
+ * @s1: size dssc s adsc
+ * @s2: sa a ass aas
+ *
+ * Return: ssd dsds
+ */
+char *str_concat(char *s1,char *s2)
+{
+	char *ptr;
+	int i, j, k;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	ptr = malloc(sizeof(char) * (i + j + 1));
+	if (ptr = NULL)
+		return (NULL);
+	for (k = 0; k < i; i++)
+	{
+		ptr[k]=s1[k];
+	}
+	for (k = i; k < (i + j); k++)
+	{
+		ptr[k] = s2[k];
+	}
+	return (ptr);
+}
