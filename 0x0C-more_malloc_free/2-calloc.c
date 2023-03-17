2555b1fd16b4ebfb82bb2af9e2220582a2fce3f0
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-17 02:00:58 -0700

    sdsd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
+	int i;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		s[i] = 0;
+	}
 	return (s);
 }
