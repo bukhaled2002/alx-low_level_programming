91c11b735b1f33d820bb1e810e09479d2c88b257
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-17 02:55:18 -0700

    fd

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int n;
 
 	if (ptr == NULL)
-		return (s);
+		return (malloc(new_size));
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
