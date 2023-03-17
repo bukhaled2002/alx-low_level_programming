f9af9d8ca224e78c307306c10a4b0631a6ab3fd5
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-17 01:53:36 -0700

    sfd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,48 +3,20 @@
 #include <stddef.h>
 #include "main.h"
 /**
- * string_nconcat - allocates memory using malloc
- * @s1: number of bytes to allocate
- * @s2: sds dsdsahdshds dsds
- * @n: sdd s sddsdf jkdf
+ * _calloc - allocates memory using malloc
+ * @mmemb: number of bytes to allocate
+ * @size: sds dsdsahdshds dsds dd s sddsdf jkdf
  *
  * Return: a pointer to the allocated memory
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, j, sum;
-	char *s;
+	void *s;
 
-	i = 0;
-	j = 0;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s2[i])
-		i++;
-	while (s1[j])
-		j++;
-	if (n > i)
-		n = i;
-	s = malloc(sizeof(char) * (j + n + 1));
-
-	sum = j + n;
-	if (!(s == NULL))
-	{
-		i = j = 0;
-		while (s1[i] != '\0')
-		{
-			s[i] = s1[i];
-			i++;
-		}
-		while (i != sum)
-		{
-			s[i] = s2[j];
-			i++;
-			j++;
-		}
-		s[i] = '\0';
-	}
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
 	return (s);
 }
