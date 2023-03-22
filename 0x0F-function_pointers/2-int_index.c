9bc189954a33a6b99f3a3e1f35eefaf3ca786436
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-22 03:10:36 -0700

    dffd

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* int_index - prints each array elem on a newl
+* @array: array
+* @size: how many elem to print
+* @cmp: pointer to prind  sd ds
+* Return: int
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || cmp == NULL)
+		return;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
