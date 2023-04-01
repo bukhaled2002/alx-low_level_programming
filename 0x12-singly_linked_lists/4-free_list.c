1dc65f3303ae9961b28b2fc998593aef95673b63
MohamedKhaled <bakhaled.310@gmail.com>
2023-03-31 18:23:26 -0700

    dd

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ *
+ * Return: void
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr = *head;
+	
+	while(ptr)
+	{
+		free(ptr);
+		ptr = ptr->next;
+	}
+}
