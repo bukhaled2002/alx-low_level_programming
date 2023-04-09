6aa708dd18af26de6c2dab9c70e8eb05c8f595a4
MohamedKhaled <bakhaled.310@gmail.com>
2023-04-09 05:18:25 -0700

    ff

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * clear_bit - converts a binary number to unsigned int
+ * @n: string containing the binary number
+ * @index: ddsds ds ds ds
+ *
+ * Return: the converted number
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	*n = (~(1 << index)) & *n;
+	return (1);
+}
