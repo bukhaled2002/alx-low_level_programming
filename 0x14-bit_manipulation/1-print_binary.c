c58732d0dc3f1090f9a505dd9f14b859b352d5e0
MohamedKhaled <bakhaled.310@gmail.com>
2023-04-09 04:29:35 -0700

    ff

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * print_binary - converts a binary number to unsigned int
+ * @n: string containing the binary number
+ *
+ * Return: the converted number
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if(!count)
+		_putchar('0');
+}
