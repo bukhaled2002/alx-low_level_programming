73be78365f19cbcf23a346d011306bbc3bbe27f5
MohamedKhaled <bakhaled.310@gmail.com>
2023-04-09 05:25:59 -0700

    ff

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * flip_bits - converts a binary number to unsigned int
+ * @n: string containing the binary number
+ * @m: ds ssd dsdsds sd
+ *
+ * Return: the converted number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned int exc = n ^ m;
+	for (i = 63; i >= 0; i--)
+	{
+		if ((exc >> i) & 1)
+			count++;
+	}
+	return count;
+}
