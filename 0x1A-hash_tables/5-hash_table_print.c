a18415943a0aaecd1a0d359d2e31515cf50bf723
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-12 02:04:24 -0700

    f

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	unsigned char comma_flag = 0;
 
-	if (!ht)
+	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
@@ -25,9 +25,9 @@ void hash_table_print(const hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				printf("%s: %s", node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
-				if (node)
+				if (node != NULL)
 					printf(", ");
 			}
 			comma_flag = 1;
