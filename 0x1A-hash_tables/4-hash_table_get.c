2ea38a58d6ac225196d54f04d976148e00cb0024
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-12 00:41:00 -0700

    f

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
-	unsigned long int i;
+	unsigned long int index;
 
 	if (!ht || !key || !*key)
 		return (NULL);
